be62d730e5e76009b554413ddec8336908fe2b13
Jerrely1618 <victorv2718@gmail.com>
2023-06-15 12:01:42 -0400

    Tested Servers

--- App.py
+++ App.py
@@ -1,4 +1,9 @@
 import tkinter as tk
+from Servers import Servers
+
+OpenServer = Servers()
+Cost = OpenServer.getStock("RBLX")
+print(f"Cost of RBLX: ${round(Cost,2)}")
 
 root = tk.Tk()
 root.geometry("1200x600")
