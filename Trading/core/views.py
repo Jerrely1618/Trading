f8439598aaa38d23cb3728b188039c0cd1c68295
Jerrely1618 <victorv2718@gmail.com>
2023-07-05 21:35:48 -0400

    Added graph and improved graphics

--- Trading/core/views.py
+++ Trading/core/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
+from utils import APIconnection
+
+financeData = APIconnection()
 
 def guest(request):
-    return render(request,'guest/GuestHome.html')
+    script,div = financeData.getStockGraph("VOO","Max")
+    context = {
+        "VOO": financeData.getStockPrice("VOO"),
+        "AAPL":financeData.getStockPrice("AAPL"),
+        "GOOGL":financeData.getStockPrice("GOOGL"),
+        "TSLA":financeData.getStockPrice("TSLA"),
+        "AMZN":financeData.getStockPrice("AMZN"),
+        "script":script,
+        "div": div,
+    }
+    return render(request,'guest/GuestHome.html',context)
 
 def login(request):
     return render(request,'guest/Login.html')
--- Trading/utils.py
+++ Trading/utils.py
@@ -1,6 +1,14 @@
 import finnhub
-import requests
+import pandas as pd
+import datetime as dt
+from time import time
 from config import Finnhub_key,Market_key
+from bokeh.io import curdoc
+from bokeh.plotting import figure
+from bokeh.themes import Theme
+from bokeh.embed import components
+from bokeh.models import NumeralTickFormatter
+from math import pi
 
 class APIconnection:
     def __init__(self) -> None:
@@ -11,10 +19,45 @@ class APIconnection:
             print(f"Unexpected {err=}, {type(err)=}")
 
     def getStockPrice(self,stock: str) -> int:
-        return self.finnhub_Conn.quote(stock)['c']
+        return round(self.finnhub_Conn.quote(stock)['c'],2)
     
-    def getStockGraph(self,stock: str):
-        return self.finnhub_Conn.stock_candles('AAPL', 'D', 0, 10000)
+    def getStockGraph(self,stock: str, from_: str):
+        graph_time={
+            "Max":[round(time()),"M"],
+            "5Y":[157680000,"M"],
+            "1Y":[31536000,"D"],
+            "6M":[14515200,"D"],
+            "1M":[2419200,"60"],
+            "5D": [604800,"30"],
+            "1D": [86400,"15"],
+        }
+        data = self.finnhub_Conn.stock_candles(symbol=stock, resolution=graph_time[from_][1] , to=round(time()), _from=round(time())-graph_time[from_][0])
+        df = self.DatatoPD(data)
+        TOOLS = "wheel_zoom, box_zoom, save"
+        curdoc().theme = Theme(filename="theme.yaml")
+        p = figure(x_axis_type="datetime", tools=TOOLS, width=1000, height=480, title = stock)
+        p.xaxis.major_label_orientation = pi / 4
+        p.grid.grid_line_alpha = 0.3
 
-conn = APIconnection()
-print(conn.getStockGraph("RBLX"))
+        p.line(df.Time,df.Cost)
+        p.yaxis[0].formatter = NumeralTickFormatter(format="$0.00")
+        curdoc().add_root(p)
+        return components(p)
+
+    def DatatoPD(self,data: dict):
+        for i,t in enumerate(data['t']):
+            data['t'][i] = dt.datetime.fromtimestamp(t).strftime('%Y-%m-%d %H:%M:%S')
+        
+        for i,c in enumerate(data['c']):
+            data['c'][i] = round(c,2)
+        
+        pandasData = list(zip(data['t'],data['c']))
+        df = pd.DataFrame(pandasData,columns=['Time','Cost'])
+        df['Time'] = pd.to_datetime(df['Time'])
+        df = df.sort_values(by=['Time'])
+        
+        df.head()
+        df.info()
+
+        return df
+    
